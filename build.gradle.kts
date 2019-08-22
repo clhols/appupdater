159bac00139b9b274221094b79d763aa989d7d05
Claus Holst <clh@visiolink.com>
2019-08-22 20:55:19 +0200

    Kotlin 1.3.50

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,12 +26,12 @@ android {
 androidExtensions { isExperimental = true }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.41")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.50")
     implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.3.0-RC2"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.11.1")
-    implementation("org.jetbrains.kotlin:kotlin-android-extensions-runtime:1.3.41")
+    implementation("org.jetbrains.kotlin:kotlin-android-extensions-runtime:1.3.50")
     implementation("androidx.appcompat:appcompat:1.1.0-rc01")
     implementation("com.squareup.okhttp3:okhttp:4.1.0")
     testImplementation("junit:junit:4.12")
