ab9bad8135de584ccbb35f1cee7d7f2ea7082e26
Claus Holst <clhols@gmail.com>
2019-08-27 22:16:31 +0200

    Coroutines 1.3.0 and serialization 0.12.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,10 +27,10 @@ androidExtensions { isExperimental = true }
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.50")
-    implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.3.0-RC2"))
+    implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.3.0"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.11.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.12.0")
     implementation("org.jetbrains.kotlin:kotlin-android-extensions-runtime:1.3.50")
     implementation("androidx.appcompat:appcompat:1.1.0-rc01")
     implementation("com.squareup.okhttp3:okhttp:4.1.0")
