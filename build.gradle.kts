f8d521714dbd5a494555abeeac609edb40e04e03
Claus Holst <clhols@gmail.com>
2020-08-18 19:10:38 +0200

    Ktor 1.4.0 etc.

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,10 +28,10 @@ androidExtensions { isExperimental = true }
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-android-extensions-runtime:1.4.0")
-    implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.3.9"))
+    implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.3.9-native-mt"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:1.0-M1-1.4.0-rc")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.0-RC")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.2.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
