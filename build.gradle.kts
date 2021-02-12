015f21f8c8df7dab0c1b846a817ebcb8ee278e1f
Claus Holst <clhols@gmail.com>
2021-02-12 23:37:39 +0100

    Kotlin 1.4.30 Compose 1.0.0-alpha12

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,6 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerVersion = Versions.kotlin
         kotlinCompilerExtensionVersion = Versions.compose
     }
 
@@ -42,14 +41,14 @@ dependencies {
     implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.4.2-native-mt"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0-RC")
     implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.2.0")
-    implementation("androidx.compose.runtime:runtime:1.0.0-alpha11")
-    implementation("androidx.compose.ui:ui-tooling:1.0.0-alpha11")
-    implementation("androidx.compose.foundation:foundation-layout:1.0.0-alpha11")
-    implementation("androidx.compose.material:material:1.0.0-alpha11")
-    implementation("dev.chrisbanes.accompanist:accompanist-insets:0.5.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.0")
+    implementation("androidx.compose.runtime:runtime:1.0.0-alpha12")
+    implementation("androidx.compose.ui:ui-tooling:1.0.0-alpha12")
+    implementation("androidx.compose.foundation:foundation-layout:1.0.0-alpha12")
+    implementation("androidx.compose.material:material:1.0.0-alpha12")
+    implementation("dev.chrisbanes.accompanist:accompanist-insets:0.5.1")
     implementation("com.squareup.okhttp3:okhttp:4.9.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
