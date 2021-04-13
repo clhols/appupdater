b4f2ae4cbba97895048ff531453891121580be69
Claus Holst <clhols@gmail.com>
2021-04-13 20:03:03 +0200

    AGP 7.0.0-alpha14 Gradle 7.0 Coil 1.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.3.0-alpha05")
+    implementation("androidx.activity:activity-compose:1.3.0-alpha06")
     implementation("dev.chrisbanes.accompanist:accompanist-insets:0.6.1")
     implementation("com.squareup.okhttp3:okhttp:4.9.1")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
