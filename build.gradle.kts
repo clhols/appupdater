d0f0b94e399bc6d5cfa2398faa7c46f6d6408b8e
Claus Holst <clhols@gmail.com>
2022-10-29 20:04:01 +0200

    Add namespace

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 }
 
 android {
+    namespace = "dk.youtec.appupdater"
     compileSdk = Versions.compileSdk
     buildToolsVersion = Versions.buildTools
 
@@ -39,9 +40,9 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${Versions.serialization}")
-    implementation("androidx.appcompat:appcompat:1.5.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
-    implementation("androidx.activity:activity-compose:1.5.1")
+    implementation("androidx.activity:activity-compose:1.6.1")
     implementation("com.squareup.okhttp3:okhttp:${Versions.okhttp}")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
